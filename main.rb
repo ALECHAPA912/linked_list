847641bf398f642634401d8a4ea04c203b28954a
Alejandro <chapale2000@gmail.com>
2025-08-25 19:22:38 -0300

    nuevo comienzo

--- main.rb
+++ main.rb
@@ -1,12 +1,2 @@
 require "./linked_list.rb"
 
-list = LinkedList.new
-
-list.append('dog')
-list.append('cat')
-list.append('parrot')
-list.append('hamster')
-list.append('snake')
-list.append('turtle')
-
-puts list
